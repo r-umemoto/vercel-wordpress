841794e052bf23e303bcbe50d89cd50cfca1763f
r-umemoto <umemoto.biz.g@gmail.com>
2025-09-28 13:30:09 +0900

    fix.

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,7 +1,21 @@
-const Spinner = () => (
-  <div className="fixed inset-0 bg-black/50 overflow-y-auto h-full w-full flex justify-center items-center z-50">
-    <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-gray-900 dark:border-gray-100"></div>
-  </div>
-);
+type SpinnerProps = {
+  fullscreen?: boolean;
+};
+
+const Spinner = ({ fullscreen = true }: SpinnerProps) => {
+  if (fullscreen) {
+    return (
+      <div className="fixed inset-0 bg-black/50 overflow-y-auto h-full w-full flex justify-center items-center z-50">
+        <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-gray-900 dark:border-gray-100"></div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex justify-center items-center p-4">
+      <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-gray-900 dark:border-gray-100"></div>
+    </div>
+  );
+};
 
 export default Spinner;
