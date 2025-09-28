9d59884aaae198081cbd81612b8447a321fdf499
r-umemoto <umemoto.biz.g@gmail.com>
2025-09-28 13:16:11 +0900

    fix.

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,5 +1,5 @@
 const Spinner = () => (
-  <div className="flex justify-center items-center p-4">
+  <div className="fixed inset-0 bg-black/50 overflow-y-auto h-full w-full flex justify-center items-center z-50">
     <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-gray-900 dark:border-gray-100"></div>
   </div>
 );
