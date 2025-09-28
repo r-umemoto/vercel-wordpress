d21b3a0c5a4af2f425a2a1fd0608be5b8c4f5b61
r-umemoto <umemoto.biz.g@gmail.com>
2025-09-28 10:22:18 +0900

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -87,7 +87,7 @@ export default function Home() {
       {/* New Detailed Search Trigger Button */}
       <button
         onClick={() => setIsMenuOpen(true)}
-        className="fixed top-1/2 -translate-y-1/2 left-0 z-30 bg-orange-500 text-white py-4 px-2 rounded-r-lg cursor-pointer hover:bg-orange-600 shadow-lg"
+        className="fixed top-1/2 -translate-y-1/2 left-0 z-30 bg-orange-500 text-white py-4 px-1 rounded-r-lg cursor-pointer hover:bg-orange-600 shadow-lg break-words"
         aria-label="詳細検索を開く"
       >
         <span style={{ writingMode: 'vertical-rl' }}>詳細検索</span>
